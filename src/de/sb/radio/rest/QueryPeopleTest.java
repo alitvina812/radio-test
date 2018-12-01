a9176e5874c0dd488adb256b580528bb930a101f
Anastasiia Litvina <al@click-solutions.de>
2018-12-01 16:37:52 +0100

    friday update

--- src/de/sb/radio/rest/QueryPeopleTest.java
+++ src/de/sb/radio/rest/QueryPeopleTest.java
@@ -27,21 +27,21 @@ public class QueryPeopleTest extends ServiceTest {
 	public void testQueryPeople() {
 		final WebTarget webTarget = newWebTarget("ines.bergmann@web.de", "ines").path("people");
 		
-		Response response = webTarget.queryParam("forename", "ines")
+		Response response = webTarget.queryParam("forename", "Ines")
 				.request(MediaType.APPLICATION_JSON_TYPE).get();
 		
 		assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
 		
-		List<Person> people = response.readEntity(new GenericType<List<Person>>() {});
+		Person[] people = response.readEntity(Person[].class);
 
 //		List<Person> people = webTarget.queryParam("forename", "ines")
 //				.request(MediaType.APPLICATION_JSON_TYPE).get(new GenericType<List<Person>>() {});
 		
 		// there should be only one "ines"
-		assertEquals(1, people.size());
+		assertEquals(1, people.length);
 		
 		// forename should be "ines" -> get Person in List at position 0 (we should have only 1)
-		assertEquals("Ines", people.get(0).getForename());
+		assertEquals("Ines", people[0].getForename());
 	}
 		
 }
